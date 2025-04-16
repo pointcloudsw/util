4dd42141d45eb0f506752b5f2edee2fcd3766933
jrollio <jrollio@outlook.com>
2025-04-16 20:31:20 +0000

    refactor locations

---
+++ loadEnv/loadEnv.go
@@ -0,0 +1,35 @@
+package util
+
+import (
+	"bufio"
+	"log"
+	"os"
+	"strings"
+)
+
+// loadEnv sets environment variables based on content input
+func loadEnv(filename string) error {
+
+	var inputString []string
+
+	if filename != "" {
+		f, e := os.Open(filename)
+		if e != nil {
+			log.Fatal("Error opening input file:", e)
+		}
+
+		defer f.Close()
+
+		scanner := bufio.NewScanner(f)
+		scanner.Split(bufio.ScanLines)
+		for scanner.Scan() {
+			inputString = strings.Split(scanner.Text(), "=")
+			os.Setenv(inputString[0], inputString[1])
+		}
+
+	} else {
+		log.Fatal("Unable to access input file: ", filename)
+
+	}
+	return nil
+}
