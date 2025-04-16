093f2bc38622459ba055363dd49b5936087b5a08
jrollio <jrollio@outlook.com>
2025-04-16 15:24:45 +0000

    initial commit

---
+++ loadEnv.go
@@ -0,0 +1,32 @@
+package util
+
+import (
+	"bufio"
+	"log"
+	"os"
+	"strings"
+)
+
+func loadEnv(filename string) error {
+
+	var inputString []string
+
+	if filename != "" {
+		f, e := os.Open(filename)
+		if e != nil {
+			log.Fatal("Error opening input file:", e)
+		}
+
+		defer f.Close()
+
+		scanner := bufio.NewScanner(f)
+		scanner.Split(bufio.ScanLines)
+		for scanner.Scan() {
+			inputString = strings.Split(scanner.Text(), "=")
+			os.Setenv(inputString[0], inputString[1])
+		}
+
+	} else {
+		log.Fatal("Unable to access input file: ", filename)
+	}
+}
